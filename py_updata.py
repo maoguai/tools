ad103fe09228563d2559ad96af9b897b84952c03
maoguai <810990206@qq.com>
2018-11-23 19:14:10 +0800

    Update py_updata.py

--- py_updata.py
+++ py_updata.py
@@ -1,1 +1,13 @@
+import psycopg2
 
+def updata_sql(model,brand,category,type1)
+    conn = psycopg2.connect(database="device", user="product", password="product", host="", port="5432")
+    print "Opened database successfully"
+    cur = conn.cursor()
+    cur.execute("UPDATE product set category = '%s' , type = '%s' where model = '%s' and brand = '%s'"
+               % (category,type1,model,brand))
+    conn.commit()
+    printf("Operation done successfully")
+    
+if __name__ == "__main__":
+    updata_sql()

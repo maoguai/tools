649cc18fe56c063ece13761dfced428f31e642cb
maoguai <810990206@qq.com>
2018-11-27 14:43:18 +0800

    Update fix_symboltable.py

--- fix_symboltable.py
+++ fix_symboltable.py
@@ -1,26 +1,21 @@
 from idaapi import *
-import time
+from idc import *
 
-#固件加载地址
-loadaddress = 0x80f0
-#符号表起始位置
-eaStart = + loadaddress
-#符号表结束位置
-eaEnd =  + loadaddress
+loadaddress = 0x
+eaStart = 0x301e64 + loadaddress
+eaEnd = 0x3293a4 + loadaddress
 
 ea = eaStart
 eaEnd = eaEnd
-
-while ea < eaEnd：
-  #循环遍历修复函数名
-  offset = 0 
-  MakeStr(Dword(ea - offset), BADADDR)
-  sName = GetString(Dword(ea - offset), -1, ASCSTR_C)
-  print(sName)
-  if sName:
-    eaFunc = Dword(ea - offset + 4)
-    MakeName(eaFunc, sName)
-    MakeCode(eaFunc)
-    MakeFunction(eaFunc, BADADDR)
+while ea < eaEnd:
+    create_strlit(Dword(ea), BADADDR)
+    sName = get_strlit_contents(Dword(ea))
+    print (sName)
+    if sName:
+        eaFunc = Dword(ea + 4)
+        MakeName(eaFunc, sName)
+        MakeCode(eaFunc)
+        MakeFunction(eaFunc, BADADDR)
+    ea = ea + 16
   ea = ea + 16
   

